acfef936d74712c0829c724ed871382704c6df41
sachinshukla636 <sachinshukla636@gmail.com>
2021-07-23 17:50:16 +0530

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
-import "./style.css";
-
+import React from 'react';
+import './style.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 export default function App() {
   return (
     <div>
-      
+      <Greet />
+      <Welcome />
     </div>
   );
 }
---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+  render() {
+    return <h2> Class components</h2>;
+  }
+}
+
+export default Welcome;
